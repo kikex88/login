26033b306e8bef65665e6440a5a1336354d9d54b
Emilio <carlos.villacortaUSAM@sv.cds>
2018-11-01 11:48:32 -0600

    login_view
    cambio de las letras en mayusculas

--- views/login.view.php
+++ views/login.view.php
@@ -22,10 +22,10 @@
         <input type="password" name="password" placeholder="Contraseña" class="form-control">
       </div>
 
-      <button type="submit" name="submit" class="btn btn-flat-green">Ingresar</button>
+      <button type="submit" name="submit" class="btn btn-flat-green">INGRESAR</button>
     </form>
 
-    <a href="#" class="login-link">¿No tienes cuenta?</a>
+    <a href="#" class="login-link">¿NO TIENE CUENTA?</a>
   </div>
 </body>
 </html>
